da9638ba45ce1df9f9b6dfcfd50ce6fa64871f3a
Nate Burley <natemburl@gmail.com>
2020-11-19 14:11:40 -0800

    Added code to generate knowledge graphs

---
+++ KnowledgeGraphs/wiki_knowledge_graph.py
@@ -0,0 +1,256 @@
+"""
+This file mines Wikipedia articles for syntax and keywords, and builds a knowledge graph.
+
+Author: Nathaniel M. Burley
+
+Notes:
+- Look at Word2Vec, and then embedding that "vector space" as a knowledge graph and
+  preserve the relations somehow...
+
+Sources:
+https://towardsdatascience.com/auto-generated-knowledge-graphs-92ca99a81121
+"""
+
+# Wikipedia scraping libraries
+import wikipediaapi  # pip install wikipedia-api
+import pandas as pd
+import concurrent.futures
+from tqdm import tqdm
+# NLP/Computational Linguistics libraries
+import re
+import spacy
+import neuralcoref
+# Graph libraries
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+
+########################################################################################################################
+## SCRAPE WIKIPEDIA REGARDING A TOPIC
+########################################################################################################################
+
+# Function that scrapes Wikipedia for a given topic
+def wikiScrape(topic_name, verbose=True):
+    def wikiLink(link):
+        try:
+            page = wiki_api.page(link)
+            if page.exists():
+                return {'page': link, 'text': page.text, 'link': page.fullurl,
+                        'categories': list(page.categories.keys())}
+        except:
+            return None
+
+    wiki_api = wikipediaapi.Wikipedia(language='en', extract_format=wikipediaapi.ExtractFormat.WIKI)
+    page_name = wiki_api.page(topic_name)
+    if not page_name.exists():
+        print('Page {} does not exist.'.format(topic_name))
+        return
+    
+    page_links = list(page_name.links.keys())
+    progress = tqdm(desc='Links Scraped', unit='', total=len(page_links)) if verbose else None
+    sources = [{'page': topic_name, 'text': page_name.text, 'link': page_name.fullurl,
+                'categories': list(page_name.categories.keys())}]
+    
+    # Parallelize the scraping, to speed it up (?)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        future_link = {executor.submit(wikiLink, link): link for link in page_links}
+        for future in concurrent.futures.as_completed(future_link):
+            data = future.result()
+            sources.append(data) if data else None
+            progress.update(1) if verbose else None     
+    progress.close() if verbose else None
+    
+    namespaces = ('Wikipedia', 'Special', 'Talk', 'LyricWiki', 'File', 'MediaWiki', 'Template', 'Help', 'User', \
+        'Category talk', 'Portal talk')
+    sources = pd.DataFrame(sources)
+    sources = sources[(len(sources['text']) > 20) & ~(sources['page'].str.startswith(namespaces, na=True))]
+    sources['categories'] = sources.categories.apply(lambda x: [y[9:] for y in x])
+    sources['topic'] = topic_name
+    print('Wikipedia pages scraped:', len(sources))
+    
+    return sources
+
+
+
+########################################################################################################################
+## COMPUTATIONAL LINGUISTICS/NLP FUNCTIONING
+# This section contains functions that do dependency parsing and find subject-predicate-object dependencies
+# TODO: FIGURE OUT HOW THIS WORKS!!!
+# TODO: Look into training a "custom entity recognizer model" to help make it domain specific
+# TODO: Look into coreference resolution to remove redundancies, normalize, etc. [adds a neural net here]
+########################################################################################################################
+
+nlp = spacy.load('en_core_web_lg')
+neuralcoref.add_to_pipe(nlp)
+
+# spacy.util.filter_spans
+def filter_spans(spans):
+    # Filter a sequence of spans so they don't contain overlaps
+    # For spaCy 2.1.4+: this function is available as spacy.util.filter_spans()
+    get_sort_key = lambda span: (span.end - span.start, -span.start)
+    sorted_spans = sorted(spans, key=get_sort_key, reverse=True)
+    result = []
+    seen_tokens = set()
+    for span in sorted_spans:
+        # Check for end - 1 here because boundaries are inclusive
+        if span.start not in seen_tokens and span.end - 1 not in seen_tokens:
+            result.append(span)
+        seen_tokens.update(range(span.start, span.end))
+    result = sorted(result, key=lambda span: span.start)
+    return result
+
+# Function that gets pairs of subjects and relationships
+def getEntityPairs(text, coref=True):
+    # preprocess text
+    text = re.sub(r'\n+', '.', text)  # replace multiple newlines with period
+    text = re.sub(r'\[\d+\]', ' ', text)  # remove reference numbers
+    text = nlp(text)
+    if coref:
+        text = nlp(text._.coref_resolved)  # resolve coreference clusters
+
+    def refineEntities(ent, sent):
+        unwanted_tokens = (
+            'PRON',  # pronouns
+            'PART',  # particle
+            'DET',  # determiner
+            'SCONJ',  # subordinating conjunction
+            'PUNCT',  # punctuation
+            'SYM',  # symbol
+            'X',  # other
+        )
+        ent_type = ent.ent_type_  # get entity type
+        if ent_type == '':
+            ent_type = 'NOUN_CHUNK'
+            ent = ' '.join(str(t.text) for t in nlp(str(ent)) if t.pos_ not in unwanted_tokens and t.is_stop == False)
+
+        elif ent_type in ('NOMINAL', 'CARDINAL', 'ORDINAL') and str(ent).find(' ') == -1:
+            refined = ''
+            for i in range(len(sent) - ent.i):
+                if ent.nbor(i).pos_ not in ('VERB', 'PUNCT'):
+                    refined += ' ' + str(ent.nbor(i))
+                else:
+                    ent = refined.strip()
+                    break
+
+        return ent, ent_type
+
+    sentences = [sent.string.strip() for sent in text.sents]  # split text into sentences
+    ent_pairs = []
+    for sent in sentences:
+        sent = nlp(sent)
+        spans = list(sent.ents) + list(sent.noun_chunks)  # collect nodes
+        spans = filter_spans(spans)
+        with sent.retokenize() as retokenizer:
+            [retokenizer.merge(span, attrs={'tag': span.root.tag, 'dep': span.root.dep}) for span in spans]
+        deps = [token.dep_ for token in sent]
+
+        # limit our example to simple sentences with one subject and object
+        # if (deps.count('obj') + deps.count('dobj')) != 1 or (deps.count('subj') + deps.count('nsubj')) != 1:
+            # continue
+
+        for token in sent:
+            if token.dep_ not in ('obj', 'dobj'):  # identify object nodes
+                continue
+            subject = [w for w in token.head.lefts if w.dep_ in ('subj', 'nsubj')]  # identify subject nodes
+            if subject:
+                subject = subject[0]
+                # identify relationship by root dependency
+                relation = [w for w in token.ancestors if w.dep_ == 'ROOT']
+                if relation:
+                    relation = relation[0]
+                    # add adposition or particle to relationship
+                    if relation.nbor(1).pos_ in ('ADP', 'PART'):
+                        relation = ' '.join((str(relation), str(relation.nbor(1))))
+                else:
+                    relation = 'unknown'
+
+                subject, subject_type = refineEntities(subject, sent)
+                token, object_type = refineEntities(token, sent)
+
+                ent_pairs.append([str(subject), str(relation), str(token), str(subject_type), str(object_type)])
+
+    ent_pairs = [sublist for sublist in ent_pairs if not any(str(ent) == '' for ent in sublist)]
+    pairs = pd.DataFrame(ent_pairs, columns=['subject', 'relation', 'object', 'subject_type', 'object_type'])
+    print('Entity pairs extracted:', str(len(ent_pairs)))
+
+    return pairs
+
+
+
+########################################################################################################################
+## FUNCTION THAT DRAWS, PLOTS THE KNOWLEDGE GRAPH
+########################################################################################################################
+
+# Function that plots and draws a knowledge graph
+def drawKG(pairs):
+    k_graph = nx.from_pandas_edgelist(pairs, 'subject', 'object', create_using=nx.MultiDiGraph())
+    node_deg = nx.degree(k_graph)
+    layout = nx.spring_layout(k_graph, k=0.15, iterations=20)
+    plt.figure(num=None, figsize=(120, 90), dpi=80)
+    nx.draw_networkx(
+        k_graph,
+        node_size=[int(deg[1]) * 500 for deg in node_deg],
+        arrowsize=20,
+        linewidths=1.5,
+        pos=layout,
+        edge_color='red',
+        edgecolors='black',
+        node_color='white',
+        )
+    labels = dict(zip(list(zip(pairs.subject, pairs.object)), pairs['relation'].tolist()))
+    nx.draw_networkx_edge_labels(k_graph, pos=layout, edge_labels=labels, font_color='red')
+    plt.axis('off')
+    plt.show()
+
+
+# Function that plots a "subgraph", if the main graph is too messy
+# Example use: filterGraph(pairs, 'Directed graphs')
+def filterGraph(pairs, node):
+    k_graph = nx.from_pandas_edgelist(pairs, 'subject', 'object', create_using=nx.MultiDiGraph())
+    edges = nx.dfs_successors(k_graph, node)
+    nodes = []
+    for k, v in edges.items():
+        nodes.extend([k])
+        nodes.extend(v)
+    subgraph = k_graph.subgraph(nodes)
+    layout = (nx.random_layout(k_graph))
+    nx.draw_networkx(
+        subgraph,
+        node_size=1000,
+        arrowsize=20,
+        linewidths=1.5,
+        pos=layout,
+        edge_color='red',
+        edgecolors='black',
+        node_color='white'
+        )
+    labels = dict(zip((list(zip(pairs.subject, pairs.object))), pairs['relation'].tolist()))
+    edges= tuple(subgraph.out_edges(data=False))
+    sublabels ={k: labels[k] for k in edges}
+    nx.draw_networkx_edge_labels(subgraph, pos=layout, edge_labels=sublabels, font_color='red')
+    plt.axis('off')
+    plt.show()
+
+
+
+########################################################################################################################
+## DO THE THING: SCRAPE WIKIPEDIA, APPLY NLP, AND BUILD A KNOWLEDGE GRAPH
+# TODO: Lots of random pages that aren't that relevant get scraped. Can those nodes be dropped later, since in theory
+#       they should be "less connected" or "weaker connected" than the other nodes that are actually "on topic"?
+########################################################################################################################
+
+# Scrape Wikipedia for a topic
+wiki_data = wikiScrape('World War II')
+print("WIKIPEDIA SCRAPE DF")
+print(wiki_data.head(25))
+print("\n")
+
+# Get subject object relationships (which form vertices and edges in the graph)
+pairs = getEntityPairs(wiki_data.loc[0,'text'])
+print("ENTITY PAIRS-- SUBJECT/OBJECT RELATIONSHIPS")
+print(pairs.head(10))
+print("\n")
+
+# Draw the graph
+drawKG(pairs)
