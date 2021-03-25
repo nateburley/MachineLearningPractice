2f9a1c351ebed7f9fcea6bd892348a4fedcbbc36
Nate Burley <nburley@Nates-MacBook-Pro.local>
2021-03-25 14:29:48 -0700

    Appears to be working for 2 hidden layers

--- ScratchNeuralNet/NN.py
+++ ScratchNeuralNet/NN.py
@@ -4,7 +4,7 @@ Contains class with neural network, and helper functions
 Author: Nathaniel M. Burley
 Date: 24th March 2021
 
-TODO: First, make it my own by adding another hidden layer
+TODO: Fix the predict function, not working with second hidden layer...
 TODO: Secondly, generalize it with loops so it works for n-layers
 """
 # Imports and constants
@@ -22,18 +22,20 @@ class NeuralNetwork(object):
     def __init__(self):
         # Layers defined (hard coded for now. Will make variable later)
         self.input_layer_size = 1
-        self.hidden_layer_size = 5
+        self.hidden_layer1_size = 5
+        self.hidden_layer2_size = 5
         self.output_layer_size = 1
 
         # Weights defined (hard coded for now. Will make list later)
-        self.W1 = np.ones((self.input_layer_size, self.hidden_layer_size))
-        self.W2 = np.ones((self.hidden_layer_size, self.output_layer_size))
+        self.W1 = np.ones((self.input_layer_size, self.hidden_layer1_size))
+        self.W2 = np.ones((self.hidden_layer1_size, self.hidden_layer2_size))
+        self.W3 = np.ones((self.hidden_layer2_size, self.output_layer_size))
 
         # Set an activation function
         self.activation_name = "sigmoid"
 
         # Set our learning rate
-        self.learning_rate = 0.1
+        self.learning_rate = 0.05
 
         # Number of training epochs
         self.num_epochs = 100
@@ -50,13 +52,15 @@ class NeuralNetwork(object):
     def displayArchitecture(self):
         print("\n------------ NETWORK ARCHITECTURE ------------")
         print("Input layer size: {}".format(self.input_layer_size))
-        print("Hidden layer size: {}".format(self.hidden_layer_size))
+        print("Hidden layer 1 size: {}".format(self.hidden_layer1_size))
+        print("Hidden layer 2 size: {}".format(self.hidden_layer2_size))
         print("Output layer size: {}".format(self.output_layer_size))
         print("Activation function: {}".format(self.activation_name))
         print("Training epochs: {}".format(self.num_epochs))
         print("Learning rate: {}".format(self.learning_rate))
         print("Weight matrix between input and hidden layer: \n{}".format(self.W1))
         print("Weight matrix between hidden and output layer: \n{}".format(self.W2))
+        print("Weight matrix between hidden and output layer: \n{}".format(self.W3))
         print("----------------------------------------------\n")
 
     
@@ -93,9 +97,16 @@ class NeuralNetwork(object):
     # Function that computes the "forward" pass, returns a prediction
     def predict(self, X):
         self.z2 = np.dot(X, self.W1)
+        #print("Shape of z2: {}".format(self.z2.shape))
         self.a2 = self.activationFunc(self.z2)
-        self.z3 = np.dot(self.a2, self.W2)
-        yHat = self.activationFunc(self.z3)
+        self.z3 = np.matmul(self.a2, self.W2)
+        print("Shape of z3: {}".format(self.z3.shape))
+        self.a3 = self.activationFunc(self.z3)
+        self.z4 = np.dot(self.a3, self.W3)  # Should this be dot? Or just multiply
+        #print("Shape of z4: {}".format(self.z4.shape))  # Why the fuck is this 1,1...
+        yHat = self.activationFunc(self.z4)
+        yHat = yHat[0,0]
+        #print("yHat shape: {}".format(yHat.shape))
         return yHat
     
 
@@ -125,34 +136,47 @@ class NeuralNetwork(object):
         self.yHat = self.predict(X)
 
         # Compute the error delta for the output layer
-        delta3 = np.multiply(-(y-self.yHat), self.activationFuncPrime(self.z3))
+        delta4 = np.multiply(-(y-self.yHat), self.activationFuncPrime(self.z4))
+        #print("Delta4 shape: {}".format(delta4.shape))
 
-        # Compute the derivative of the cost function with respect to W2
+        # Compute the derivative of the cost function with respect to W3
+        dJdW3 = np.dot(self.a3.T, delta4)
+        #print("dJdW3 shape: {}".format(dJdW3.shape))
+
+        # Compute the error for the second hidden layer
+        delta3 = np.dot(delta4, self.W3.T) * self.activationFuncPrime(self.z3)
+        #print("Delta3 shape: {}".format(delta3.shape))
+
+        # Compute the derivative with respect to W2
         dJdW2 = np.dot(self.a2.T, delta3)
+        #print("dJdW2 shape: {}".format(dJdW2.shape))
 
         # Compute the error delta for the hidden layer
-        delta2 = np.dot(delta3, self.W2.T) * self.activationFuncPrime(self.z2)
+        delta2 = np.matmul(delta3, self.W2.T) * self.activationFuncPrime(self.z2)
+        #print("Delta2 shape: {}".format(delta2.shape))
 
         # Compute the derivative of the cost function with respect to W1
         dJdW1 = np.dot(X.T, delta2)
+        #print("dJdW1 shape: {}".format(dJdW1.shape))
 
         # Output the gradients
-        return dJdW1, dJdW2
+        return dJdW1, dJdW2, dJdW3
     
 
     # Function that updates the weights
     # TODO: Replace this with a 'gradient descent' and actually update the weights to train...
     def updateWeights(self, X, y):
         # Compute the gradients
-        dJdW1, dJdW2 = self.costFuncPrime(X, y)
+        dJdW1, dJdW2, dJdW3 = self.costFuncPrime(X, y)
 
         # Update the weights by simply subtracting the gradients in a step of the learning rate
         self.W1 = self.W1 - (self.learning_rate * dJdW1)
         self.W2 = self.W2 - (self.learning_rate * dJdW2)
+        self.W3 = self.W3 - (self.learning_rate * dJdW3)
 
     
     # Function that trains on a complete dataset (or batch, I suppose. Multiple samples.)
-    def train(self, X_train, y_train, num_epochs=200):
+    def train(self, X_train, y_train, num_epochs=100):
         for epoch in range(0, self.num_epochs):
             for X, y in zip(X_train, y_train):
                 self.updateWeights(X, y)
